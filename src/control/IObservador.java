60b0716b6f35cf72d9caccc367117ef10fa68783
Ignacio Hernandez <hernandezci.2007@gmail.com>
2026-09-24 15:03:08 -0600

    Agregar interfaz IObservador para el patrón Observer

---
+++ src/control/IObservador.java
@@ -0,0 +1,7 @@
+package control;
+
+// Patrón Observer: quien quiera enterarse de los cambios de la batalla debe implementar esta interfaz.
+public interface IObservador {
+
+    public void actualizar();
+}
